871cc7ca221727b21df6911761b66ca4a78f231b
cuongb48 <hungcuongb48@gmail.com>
2025-10-16 17:38:45 +0700

    up

--- build/server.js
+++ build/server.js
@@ -47,10 +47,8 @@ app.use(cors({
 app.options('*', cors()); // Enable preflight requests for all routes
 
 // Config bodyParser
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 
 // Đặt thư mục public/uploads làm public để có thể truy cập
 app.use('/uploads', express["static"](path.join(__dirname, './public/uploads')));
